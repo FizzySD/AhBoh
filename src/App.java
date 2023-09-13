50de6d3e3e1369af0adbb9ec1304b6c5f522688d
FizzySD <64667188+FizzySD@users.noreply.github.com>
2023-09-13 15:52:59 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.security.PublicKey;
+import java.sql.*;
+
+
+public class App 
+{
+    public static DatabaseHadler db = new DatabaseHadler();
+    public static void main(String[] args) throws Exception 
+    {
+
+    }
+}
---
+++ src/DatabaseHadler.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+
+public class DatabaseHadler 
+{
+    public static DatabaseHadler instance = null; 
+    static final String DB_URL = "jdbc:mysql://localhost/aottg_maps";
+    static final String USER = "root";
+    static final String PASS = "";
+    public Statement stmt;
+    //static final String QUERY = "SELECT id, first, last, age FROM Employees";
+    
+    public DatabaseHadler()
+    {
+        instance = this;
+        DBConnect();
+    }
+
+    public void DBConnect()
+    {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt = conn.createStatement();)
+        {
+        this.stmt = stmt;
+        System.out.println("DB connesso");
+        ExecuteQuery("SELECT MapName FROM maps WHERE ID = 1");
+        }catch (SQLException e)
+        {
+        e.printStackTrace();
+        System.out.println("DB non connesso");
+        } 
+    }
+
+    public void ExecuteQuery(String Query)
+    {
+        try
+        {
+            ResultSet rs = stmt.executeQuery(Query);
+            while(rs.next())
+            {
+                System.out.println("Mappa Scelta: " + rs.getString("MapName"));
+            }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
